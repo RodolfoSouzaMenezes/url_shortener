c857615d19cdff7e95a53d58a377f67fbe3155c1
Rodolfo Souza Menezes <91015324+RodolfoSouzaMenezes@users.noreply.github.com>
2022-03-10 14:46:15 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://dbUser:l0405t1a@cluster0.tc0he.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		'mongodb+srv://dbUser:***********.tc0he.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
