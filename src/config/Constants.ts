1bafd73ef6adf89bf6ba0cbdc41c979851e74daa
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-03-10 14:04:22 -0300

    Final Commit

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,5 @@
 export const config = {
-    API_URL: "http://localhost:5000"
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://dbUser:l0405t1a@cluster0.tc0he.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
